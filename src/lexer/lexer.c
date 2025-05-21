b71b404ba079a48b4663f6c5987db038dda1e8d0
mviana-v <matheus.viuge05@gmail.com>
2025-05-21 18:07:25 -0300

    lógica inicial lexer

--- src/lexer/lexer.c
+++ src/lexer/lexer.c
@@ -6,36 +6,37 @@
 /*   By: mviana-v <mviana-v@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/14 18:57:09 by mviana-v          #+#    #+#             */
-/*   Updated: 2025/05/20 15:24:58 by mviana-v         ###   ########.fr       */
+/*   Updated: 2025/05/21 17:53:24 by mviana-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-bool	verify_word(t_token *token, t_env **env);
+static bool	verify_word(t_token *token, t_env **env);
 
-void	print_error(char *msg, bool *is_valid)
+static void	print_error(char *msg, bool *is_valid)
 {
 	ft_putendl_fd(msg, 2);
 	*is_valid = false;
 }
 
-bool	verify_metas(t_token *token, t_env **env)
+static void	verify_metas(t_token *token, bool *is_valid)
 {
-	bool	is_valid;
-
-	is_valid = true;
 	if (token->type == REDIR_IN || token->type == REDIR_OUT)
 	{
 		if (token->next == NULL || token->next->type != WORD)
-			print_error("syntax error: unexpected token", &is_valid);
+			print_error("syntax error: unexpected token", is_valid);
 	}
 	else if (token->type == HEREDOC || token->type == APPEND)
 	{
 		if (token->next == NULL || token->next->type != WORD)
-			print_error("syntax error: unexpected token", &is_valid);
+			print_error("syntax error: unexpected token", is_valid);
 	}
-	return (is_valid);
+	else if (token->type == PIPE)
+	{
+		if (token->next == NULL || token->next == NULL)
+			print_error("syntax error: unexpected token", is_valid);
+	}
 }
 
 bool	lexer(t_token **token, t_env **env)
@@ -45,12 +46,12 @@ bool	lexer(t_token **token, t_env **env)
 
 	tmp = *token;
 	is_valid = true;
-	while (tmp)
+	while (tmp && is_valid)
 	{
 		if (tmp->type == WORD) //TO DO: Trocar a funcao que verifica se o token é um word, pela de expansao de variaveis
 			is_valid = verify_word(tmp, env);
 		else
-			is_valid = verify_metas(tmp, env);
+			verify_metas(tmp, &is_valid);
 		tmp = tmp->next;
 	}
 		
