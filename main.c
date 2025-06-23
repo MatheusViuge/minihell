52020e39fca5d97a97f11b3f9edb8145143f3d54
mviana-v <matheus.viuge05@gmail.com>
2025-06-23 19:46:06 -0300

    Aadicionada funcao para free da AST

--- main.c
+++ main.c
@@ -3,14 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mviana-v <mviana-v@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mviana-v <mviana-v@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 23:20:52 by jesda-si          #+#    #+#             */
-/*   Updated: 2025/05/27 23:14:39 by mviana-v         ###   ########.fr       */
+/*   Updated: 2025/06/20 19:49:28 by mviana-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./include/minishell.h"
+
+void	free_data(t_data *data)
+{
+	if (data->env)
+		free_env(data->env);
+	if (data->tokens)
+		free_tokens(&data->tokens);
+	if (data->ast)
+		free_ast(data->ast);
+}
 
 int	main(int ac, char **av, char **env)
 {
@@ -39,7 +49,7 @@ int	main(int ac, char **av, char **env)
 		free_tokens(&data.tokens);
 	}
 	clear_history();
-	free_env(data.env);
+	free_data(&data);
 	return (data.exit_code);
 }
 
