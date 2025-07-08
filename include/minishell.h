93997e72b862fa027dc0ca64f0dfdcdf65e45a4f
mviana-v <matheus.viuge05@gmail.com>
2025-07-07 22:13:30 -0300

    Adicionar funções de tratamento de redirect e atualizar o arquivo de cabeçalho

--- include/minishell.h
+++ include/minishell.h
@@ -6,7 +6,7 @@
 /*   By: mviana-v <mviana-v@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/14 23:21:07 by jesda-si          #+#    #+#             */
-/*   Updated: 2025/07/01 18:17:13 by mviana-v         ###   ########.fr       */
+/*   Updated: 2025/07/03 18:41:39 by mviana-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,7 @@ void			create_env(char **env, t_env **head);
 void			print_env(t_env *head);
 void			free_env(t_env *head);
 
-/*  token functions  */
+/*	token functions	*/
 bool			token(t_data *data, char *prompt);
 char			*end_token(char *str);
 t_type_token	type_token(char *token);
@@ -50,7 +50,7 @@ int				token_quote(char *str, char **end);
 char			*token_meta_char(char *end);
 void			free_tokens(t_token **tokens);
 
-/*  expanding variables */
+/*	expanding variables	*/
 bool			expand_variable(t_token *token, t_data *data);
 char			*replace_variable(char *value, int *index, t_data *data);
 char			*token_recreate(char *value, char *variable, int *index,
@@ -58,10 +58,10 @@ char			*token_recreate(char *value, char *variable, int *index,
 char			*find_key(char *variable, t_env *env);
 int				keycmp(char *variable, t_env env, char **str);
 
-/* lexer functions */
+/*	lexer functions	*/
 bool			lexer(t_token **tokens, t_data *data);
 
-/*  parser functions */
+/*	parser functions	*/
 bool			parser(t_data *data, char *input);
 bool			is_redir(t_token *token);
 void			free_ast(t_node **ast);
@@ -78,7 +78,10 @@ int				next_type_token(t_token *token, int count);
 t_type_token	get_node_type(char *cmd);
 t_redir			*new_redir(t_token *token);
 
-/*  para apagar      */
+/*	redirects functios	*/
+void	handle_redirects(t_data *data, t_node *node);
+
+/*	para apagar	*/
 void			print_tokens(t_token *tokens);
 bool			exec_command(t_data *data, char *command);
 bool			return_erro(char *message, int code, t_data *data);
