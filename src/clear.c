363c3c14b8a344e37faae69b4bdcbcca728d262d
jessica <jessicasoaresdev@gmail.com>
2025-07-07 23:07:21 -0300

    trazendo funcao de limpeza para arquivo clear

--- src/clear.c
+++ src/clear.c
@@ -6,7 +6,7 @@
 /*   By: jesda-si <jesda-si@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/20 18:32:33 by jesda-si          #+#    #+#             */
-/*   Updated: 2025/07/07 19:17:50 by jesda-si         ###   ########.fr       */
+/*   Updated: 2025/07/07 19:24:40 by jesda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,6 +51,19 @@ void	free_env(t_env **head)
 	}
 	free(current);
 	*head = NULL;
+}
+
+static void	free_redir_list(t_redir *head)
+{
+	t_redir	*tmp;
+
+	while (head)
+	{
+		tmp = head->next;
+		free(head->name);
+		free(head);
+		head = tmp;
+	}
 }
 
 void	free_ast(t_node **ast)
