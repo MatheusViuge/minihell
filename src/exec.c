63d5cabd3c013111abfbc0949ca3d578e4b8c1a1
jessica <jessicasoaresdev@gmail.com>
2025-07-07 23:24:06 -0300

    matando print tokens

--- src/exec.c
+++ src/exec.c
@@ -6,7 +6,7 @@
 /*   By: jesda-si <jesda-si@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 22:04:39 by jesda-si          #+#    #+#             */
-/*   Updated: 2025/05/07 22:04:42 by jesda-si         ###   ########.fr       */
+/*   Updated: 2025/07/07 23:04:51 by jesda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@ bool	exec_command(t_data *data, char *command)
 	// DEFINIR TRATAMENTO DE ERRO
 	token(data, command);
 	lexer(data);
-	print_tokens(data->tokens);
 
 	// SOMENTE PARA TESTE
 	if (size_tokens(data->tokens) == 1
