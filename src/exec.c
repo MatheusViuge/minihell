8f9bd82b8669916bf7764c3e6a74b8b5e8e83c70
mviana-v <matheus.viuge05@gmail.com>
2025-07-24 02:47:06 -0300

    Falta lidar com os builtins, tratamento de sinal, ainda nao foi testado

--- src/exec.c
+++ src/exec.c
@@ -6,7 +6,7 @@
 /*   By: mviana-v <mviana-v@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/07 22:04:39 by jesda-si          #+#    #+#             */
-/*   Updated: 2025/07/11 17:54:46 by mviana-v         ###   ########.fr       */
+/*   Updated: 2025/07/22 20:22:39 by mviana-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,16 @@ void	dupper(int fd_in, int fd_out)
 	}
 }
 
+static void	exec_cleaner(t_data *data, char **path)
+{
+	int	exit_code;
+
+	path_cleaner(path);
+	exit_code = data->exit_code;
+	free_data(data);
+	exit(exit_code);
+}
+
 void	exec(t_data *data, t_node *node, char **path)
 {
 	int	i;
@@ -37,15 +47,10 @@ void	exec(t_data *data, t_node *node, char **path)
 			break ;
 		i++;
 	}
-	if (path[i] == NULL)
-	{
-		return_erro(data, 127, "Command not found");
-		exit(data->exit_code);
-	}
+	if (!path[i])
+		exec_exit(data, 127, "Command not found");
 	dupper(node->fd_in, node->fd_out);
 	if (execve(path[i], node->cmd, data->env) == -1)
-	{
 		return_erro(data, 1, "Execution failed");
-		exit(data->exit_code);
-	}
+	exec_cleaner(data, path);
 }
--- src/execucao/exec_handler.c
+++ src/execucao/exec_handler.c
@@ -6,18 +6,51 @@
 /*   By: mviana-v <mviana-v@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/09 20:22:27 by mviana-v          #+#    #+#             */
-/*   Updated: 2025/07/11 17:45:18 by mviana-v         ###   ########.fr       */
+/*   Updated: 2025/07/24 02:22:06 by mviana-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
-void	exec_pipe(t_data *data, t_node *ast)
+static void	ast_fd_closer(t_node *ast)
 {
-	
+	if (!ast)
+		return ;
+	ast_fd_closer(ast->left);
+	ast_fd_closer(ast->right);
+	if (ast->fd_in != -1)
+		close(ast->fd_in);
+	if (ast->fd_out != -1)
+		close(ast->fd_out);
 }
 
-void	single_exec(t_data *data, t_node *node)
+static void	exec_pipe(t_data *data, t_node *ast)
+{
+	int		pid;
+	char	**path;
+
+	if (!ast)
+		return ;
+	if (ast->type == PIPE)
+	{
+		exec_pipe(data, ast->left);
+		exec_pipe(data, ast->right);
+		return ;
+	}
+	pid = fork();
+	if (pid < 0)
+	{
+		return_erro(data, 1, "Fork failed");
+		return ;
+	}
+	path = path_finder(data->env, ast->cmd[0]);
+	if (pid == 0)
+		exec(data, ast, path);
+	path_cleaner(path);
+	pid_handler(data, pid);
+}
+
+static void	single_exec(t_data *data, t_node *node)
 {
 	int		pid;
 	int		status;
@@ -37,7 +70,8 @@ void	single_exec(t_data *data, t_node *node)
 	}
 	if (pid == 0)
 		exec(data, node, path);
-	waitpid(pid, &status);
+	pid_handler(data, pid);
+	path_cleaner(path);
 	data->exit_code = WEXITSTATUS(status);
 }
 
@@ -45,15 +79,14 @@ void	exec_handler(t_data *data)
 {
 	if (data->exit_code != 0)
 		return ;
-	if (data->ast == NULL)
-	{
-		data->exit_code = 0;
+	if (!data->ast)
 		return ;
-	}
 	handle_redirects(data, data->ast);
 	handle_pipes(data->ast);
 	if (data->ast->type == PIPE)
 		exec_pipe(data, data->ast);
 	else
 		single_exec(data, data->ast);
+	pid_wait(data, data->pids);
+	ast_fd_closer(data->ast);
 }
