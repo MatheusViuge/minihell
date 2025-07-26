9f502885bdc104ea50bdb11a92b46e78eefc3873
jessica <jessicasoaresdev@gmail.com>
2025-07-26 12:58:09 -0300

    tirando while infinito

--- src/builtins/env/utils.c
+++ src/builtins/env/utils.c
@@ -6,7 +6,7 @@
 /*   By: jesda-si <jesda-si@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 19:14:45 by jesda-si          #+#    #+#             */
-/*   Updated: 2025/06/28 19:32:05 by jesda-si         ###   ########.fr       */
+/*   Updated: 2025/07/26 12:57:16 by jesda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,10 @@ t_env	*find_env(char *variable, t_env *env)
 	while (!node && tmp != env)
 	{
 		if (!ft_strncmp(variable, tmp->key, ft_strlen(variable) + 1))
+		{
 			node = tmp;
-		if (!node)
-			continue ;
+			break ;
+		}
 		tmp = tmp->next;
 	}
 	return (node);
