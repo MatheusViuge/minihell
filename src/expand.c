625bbf0d3e9c511921f296b7f39dafaba7acd924
jessica <jessicasoaresdev@gmail.com>
2025-08-22 15:12:34 -0300

    ops

--- src/expand.c
+++ src/expand.c
@@ -6,7 +6,7 @@
 /*   By: jesda-si <jesda-si@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/03 20:02:08 by jesda-si          #+#    #+#             */
-/*   Updated: 2025/08/22 15:06:09 by jesda-si         ###   ########.fr       */
+/*   Updated: 2025/08/22 15:12:13 by jesda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ bool	expand_variable(t_token *token, t_data *data)
 		if ((token->value[i] == '\'' || token->value[i] == '\"'))
 			set_quote(&token->value, &i, &quote, token->value[i]);
 		if (!token->value)
-			return (return_erro("Error", 2, data));
+			return (return_erro("Error", NULL, 2, data));
 		if (token->value[i] != '$' || (token->value[i] == '$' && quote == 1))
 			continue ;
 		token->value = replace_variable(token->value, &i, data);
@@ -39,7 +39,7 @@ bool	expand_variable(t_token *token, t_data *data)
 		if (token->value[i] == '\'' || token->value[i] == '\"')
 			set_quote(&token->value, &i, &quote, token->value[i]);
 		if (!token->value)
-			return (return_erro("Error", 2, data));
+			return (return_erro("Error", NULL, 2, data));
 	}
 	return (true);
 }
