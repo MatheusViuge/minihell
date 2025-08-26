9fd503c060ef99b320df78791c9123f727d4173d
mviana-v <matheus.viuge05@gmail.com>
2025-08-25 21:52:57 -0300

    agora o informatico fez funcionar

--- src/execucao/path_utils.c
+++ src/execucao/path_utils.c
@@ -6,7 +6,7 @@
 /*   By: mviana-v <mviana-v@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 19:05:44 by mviana-v          #+#    #+#             */
-/*   Updated: 2025/08/20 12:21:12 by mviana-v         ###   ########.fr       */
+/*   Updated: 2025/08/25 21:51:34 by mviana-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,20 @@ void	path_join(char **path, char *cmd)
 	}
 }
 
+static char	**get_single_path(char **path, char *cmd)
+{
+	path = ft_calloc(sizeof(char *), 2);
+	if (!path)
+		return (NULL);
+	path[0] = ft_strdup(cmd);
+	if (!path[0])
+	{
+		free(path);
+		return (NULL);
+	}
+	return (path);
+}
+
 char	**path_finder(t_env *env, char *cmd)
 {
 	char	**path;
@@ -64,6 +78,8 @@ char	**path_finder(t_env *env, char *cmd)
 		return (NULL);
 	current = env->next;
 	path = NULL;
+	if (access(&cmd[0], F_OK | X_OK) == 0)
+		return (get_single_path(path, &cmd[0]));
 	while (current != env)
 	{
 		if (ft_strncmp(current->key, "PATH", 4) == 0)
