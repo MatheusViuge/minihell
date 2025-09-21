a58249e8d2ce2226c7937d6e8caec39c410a7905
mviana-v <matheus.viuge05@gmail.com>
2025-09-21 00:12:26 -0300

    correcao do cd ..

--- src/builtins/cd.c
+++ src/builtins/cd.c
@@ -3,17 +3,17 @@
 /*                                                        :::      ::::::::   */
 /*   cd.c                                               :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jesda-si <jesda-si@student.42.rio>         +#+  +:+       +#+        */
+/*   By: mviana-v <mviana-v@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/28 21:13:17 by jesda-si          #+#    #+#             */
-/*   Updated: 2025/09/20 21:37:09 by jesda-si         ###   ########.fr       */
+/*   Updated: 2025/09/21 00:10:59 by mviana-v         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/minishell.h"
 
 static char	*create_pwd(char *arg, t_env *env);
-static char	**create_variables(char *pwd);
+static char	**create_variables(char **pwd, char *arg, t_env *env);
 
 void	cd(char **args, t_env **env)
 {
@@ -23,15 +23,16 @@ void	cd(char **args, t_env **env)
 
 	if (len_args(args) > 1)
 	{
-		printf("too many arguments\n");
+		ft_putendl_fd("too many arguments", 2);
 		return ;
 	}
-	pwd = create_pwd(args[0], *env);
-	strs = create_variables(pwd);
-	if (!strs)
+	strs = create_variables(&pwd, args[0], *env);
+	if (access(pwd, F_OK) != 0)
 	{
+		perror("cd");
 		free(pwd);
-		exit_error(NULL, NULL, NULL);
+		free_split(&strs);
+		return ;
 	}
 	ret = chdir(pwd);
 	if (ret == 0)
@@ -43,23 +44,32 @@ void	cd(char **args, t_env **env)
 	return ;
 }
 
-static char	**create_variables(char *pwd)
+static char	**create_variables(char **pwd, char *arg, t_env *env)
 {
 	char	*str;
 	char	**strs;
 	char	*old_pwd;
 
-	if (!pwd)
-		return (NULL);
+	*pwd = create_pwd(arg, env);
 	old_pwd = getcwd(NULL, 0);
 	if (!old_pwd)
-		return (NULL);
-	str = ft_sprintf("OLDPWD=%s PWD=%s", old_pwd, pwd);
+	{
+		perror("cd");
+		old_pwd = ft_strdup("");
+		if (!old_pwd)
+			exit_error(NULL, NULL, NULL);
+	}
+	str = ft_sprintf("OLDPWD=%s PWD=%s", old_pwd, *pwd);
 	free(old_pwd);
 	if (!str)
-		return (NULL);
+		exit_error(NULL, NULL, NULL);
 	strs = ft_split(str, 32);
 	free(str);
+	if (!strs)
+	{
+		free(*pwd);
+		exit_error(NULL, NULL, NULL);
+	}
 	return (strs);
 }
 
@@ -73,6 +83,8 @@ static char	*create_pwd(char *arg, t_env *env)
 		pwd = find_value_env("OLDPWD", env);
 	else
 		pwd = ft_strdup(arg);
+	if (!pwd)
+		exit_error(NULL, NULL, NULL);
 	return (pwd);
 }
 
