4bebc0c73320e667e1acadead51e541c1ccee81a
jessica <jessicasoaresdev@gmail.com>
2025-09-24 23:03:31 -0300

    teste para tokenizacao

--- src/Parser/parser_main.c
+++ src/Parser/parser_main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser_main.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mviana-v <mviana-v@student.42.rio>         +#+  +:+       +#+        */
+/*   By: jesda-si <jesda-si@student.42.rio>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/17 16:59:59 by mviana-v          #+#    #+#             */
-/*   Updated: 2025/09/20 22:48:00 by mviana-v         ###   ########.fr       */
+/*   Updated: 2025/09/24 23:03:04 by jesda-si         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,46 @@
 bool	is_redir(t_token *token)
 {
 	return (token->type != PIPE && token->type != WORD);
+}
+
+void	print_tokens(t_token *tokens)
+{
+	t_token	*tmp;
+
+	tmp = tokens;
+	while (tmp)
+	{
+		switch (tmp->type)
+		{
+		case WORD:
+			printf("type: WORD        token: %s\n", tmp->value);
+			break;
+		case PIPE:
+			printf("type: PIPE        token: %s\n", tmp->value);
+			break;
+		case REDIR_IN:
+			printf("type: REDIR_IN    token: %s\n", tmp->value);
+			break;
+		case REDIR_OUT:
+			printf("type: REDIR_OUT   token: %s\n", tmp->value);
+			break;
+		case APPEND:
+			printf("type: APPEND      token: %s\n", tmp->value);
+			break;
+		case HEREDOC:
+			printf("type: HEREDOC     token: %s\n", tmp->value);
+			break;
+		case AND:
+			printf("type: AND         token: %s\n", tmp->value);
+			break;
+		case OR:
+			printf("type: OR          token: %s\n", tmp->value);
+			break;
+		default:
+			break;
+		}
+		tmp = tmp->next;
+	}
 }
 
 bool	parser(t_data *data, char *input)
